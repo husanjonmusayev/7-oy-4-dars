1494873c0200026985de39966ac8ac4322b3a0ae
husanjonmusayev <husanjonm279@gmail.com>
2024-02-29 03:21:46 +0500

    react weather

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -65,9 +65,21 @@ const Thum = styled.div`
   align-items: center;
 `;
 
+const Loader = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+`;
+
 function Home() {
   const [citiy, setSitiy] = useState();
   const cityRef = useRef();
+  const [loader, setLoader] = useState(false);
+
+  // get data location
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -76,13 +88,19 @@ function Home() {
           `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=56ff80ac3a0c0f5f2e37a0de8ad98800&units=metric`
         )
           .then((res) => res.json())
-          .then((data) => setSitiy(data));
+          .then((data) => {
+            if (data) {
+              setSitiy(data);
+              setLoader(true);
+            }
+          });
       });
     } else {
       console.log("Geolocation is not available.");
     }
   }, []);
 
+  //  get data city name
   function handlsubmit(event) {
     if (event.charCode == 13) {
       if (cityRef.current.value.length) {
@@ -97,60 +115,82 @@ function Home() {
               alert("shaxar nomi noto'g'ri kiritildi");
             }
           })
-          .catch((err) => console.log( err));
+          .catch((err) => console.log(err));
       } else {
         alert("Shaxar nomini kiriting");
       }
       cityRef.current.value = "";
     }
   }
+
+  // loader function
+
   return (
-    <Card>
-      <Header>
-        <Ul>
-          <li>
-            <H1>Cauntry : {citiy ? citiy.sys.country : " "} </H1>
-          </li>
-          <li>
-            <H2>Name : {citiy ? citiy.name : " "}</H2>
-          </li>
-        </Ul>
-        <div className="search">
-          <div class="group">
-            <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
-              <g>
-                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-              </g>
-            </svg>
-            <input
-              ref={cityRef}
-              class="input"
-              type="search"
-              onKeyPress={handlsubmit}
-              placeholder="Search"
-            />
+    <>
+      {loader ? (
+        <Card>
+          <Header>
+            <Ul>
+              <li>
+                <H1>Cauntry : {citiy ? citiy.sys.country : " "} </H1>
+              </li>
+              <li>
+                <H2>Name : {citiy ? citiy.name : " "}</H2>
+              </li>
+            </Ul>
+            <div className="search">
+              <div class="group">
+                <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
+                  <g>
+                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                  </g>
+                </svg>
+                <input
+                  ref={cityRef}
+                  class="input"
+                  type="search"
+                  onKeyPress={handlsubmit}
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+          </Header>
+          <Main>
+            <Thum>
+              <img
+                src={`https://openweathermap.org/img/wn/${
+                  citiy ? citiy.weather[0].icon : " "
+                }@4x.png`}
+                alt=""
+              />
+              <H2>{citiy ? citiy.weather[0].main : ""}</H2>
+            </Thum>
+          </Main>
+          <section>
+            <Col className="col">
+              <Item>Temprature : {citiy ? citiy.main.temp : ""} ℃</Item>
+              <Item>Max temp : {citiy ? citiy.main.temp_max : ""} ℃</Item>
+              <Item>Min temp : {citiy ? citiy.main.temp_min : ""} ℃</Item>
+            </Col>
+          </section>
+        </Card>
+      ) : (
+        <Loader>
+          <div class="container">
+            <div class="cloud front">
+              <span class="left-front"></span>
+              <span class="right-front"></span>
+            </div>
+            <span class="sun sunshine"></span>
+            <span class="sun"></span>
+            <div class="cloud back">
+              <span class="left-back"></span>
+              <span class="right-back"></span>
+            </div>
           </div>
-        </div>
-      </Header>
-      <Main>
-        <Thum>
-          <img
-            src={`https://openweathermap.org/img/wn/${
-              citiy ? citiy.weather[0].icon : " "
-            }@4x.png`}
-            alt=""
-          />
-          <H2>{citiy ? citiy.weather[0].main : ""}</H2>
-        </Thum>
-      </Main>
-      <section>
-        <Col className="col">
-          <Item>Temprature : {citiy ? citiy.main.temp : ""} ℃</Item>
-          <Item>Max temp : {citiy ? citiy.main.temp_max : ""} ℃</Item>
-          <Item>Min temp : {citiy ? citiy.main.temp_min : ""} ℃</Item>
-        </Col>
-      </section>
-    </Card>
+        </Loader>
+      )}
+    </>
   );
 }
 
--- src/Redux/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-export const getApi = configureStore({
-  reducer: {
-    data: "data",
-  },
-});
